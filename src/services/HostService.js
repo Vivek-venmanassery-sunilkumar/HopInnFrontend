9181ad17355cc6837fc2f7ac10c1ed605983e7a0
kid <venmanassery.vivek@gmail.com>
2025-10-01 15:19:38 +0530

    made a service to edit host

--- src/services/HostService.js
+++ src/services/HostService.js
@@ -25,3 +25,16 @@ export const fetchHostProfile = async ()=>{
         throw new Error(serverMessage || error.message)
     }
 }
+
+
+export const editHostProfile = async (data) =>{
+    try{
+        const response = await authApi.put('/host-profile/update', {...data});
+        if(response.status === 200){
+            return response.data
+        }
+    }catch(error){
+        const serverMessage = error.response?.data?.detail?.message;
+        throw new Error(serverMessage || error.message)
+    }
+}
